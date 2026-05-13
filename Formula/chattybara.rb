ee94ed4225d116151614c81a99729ac8e6ecd008
nvk <git@rnvk.org>
2026-05-13 10:41:47 -0600

    update chattybara to 0.1.0-alpha.7

--- Formula/chattybara.rb
+++ Formula/chattybara.rb
@@ -1,12 +1,12 @@
 class Chattybara < Formula
   desc "Terminal radio chat client with the clean-room orca modem stack"
   homepage "https://github.com/nvk/chattybara"
-  version "0.1.0-alpha.6"
+  version "0.1.0-alpha.7"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/nvk/chattybara/releases/download/v0.1.0-alpha.6/chattybara-0.1.0-alpha.6-aarch64-apple-darwin.tar.gz"
-    sha256 "7ad876c6b2ba974188204f457bc3fa0dacae0d20b6fed5b0452a773291d8f7c7"
+    url "https://github.com/nvk/chattybara/releases/download/v0.1.0-alpha.7/chattybara-0.1.0-alpha.7-aarch64-apple-darwin.tar.gz"
+    sha256 "ec36f9c5fa55eeebfacf66410f6f2f70a352c1c8d18253c9e714467b1b3d69ea"
   else
     raise "prebuilt chattybara Homebrew formula currently supports Apple Silicon macOS only"
   end
