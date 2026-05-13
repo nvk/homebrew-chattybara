344677d9079e3fc0454e30dc7051f36ea2d97b17
nvk <git@rnvk.org>
2026-05-13 08:51:56 -0600

    update chattybara to 0.1.0-alpha.4

--- Formula/chattybara.rb
+++ Formula/chattybara.rb
@@ -1,12 +1,12 @@
 class Chattybara < Formula
   desc "Terminal radio chat client with the clean-room orca modem stack"
   homepage "https://github.com/nvk/chattybara"
-  version "0.1.0-alpha.3"
+  version "0.1.0-alpha.4"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/nvk/chattybara/releases/download/v0.1.0-alpha.3/chattybara-0.1.0-alpha.3-aarch64-apple-darwin.tar.gz"
-    sha256 "1ee233343baac746c275fc971866f4bc1e1009d5cafd07dd089dc6be2ffc54a0"
+    url "https://github.com/nvk/chattybara/releases/download/v0.1.0-alpha.4/chattybara-0.1.0-alpha.4-aarch64-apple-darwin.tar.gz"
+    sha256 "43f1a4f2901fda7b6bfbc71c39b9e90d9984f965a26935ff4344fd2c6e9c4773"
   else
     raise "prebuilt chattybara Homebrew formula currently supports Apple Silicon macOS only"
   end
@@ -19,7 +19,10 @@ class Chattybara < Formula
     system "#{bin}/chattybara", "--help"
     output = shell_output("#{bin}/chattybara modem roundtrip \"hello chattybara\"")
     assert_match '"ok": true', output
-    output = shell_output("#{bin}/chattybara winlink telnet --station JA1TST --check")
+    settings = testpath/"settings.toml"
+    system "#{bin}/chattybara", "station", "config", "--station", "JA1TST", "--path", settings
+    ENV["CHATTYBARA_SETTINGS"] = settings.to_s
+    output = shell_output("#{bin}/chattybara winlink telnet --check")
     assert_match '"transport": "telnet-cms"', output
   end
 end
